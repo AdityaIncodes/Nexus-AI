a56dbaede239dbe3b10ba65abe1498f184aaaa49
AdityaIncodes <adityasinghaks101o@gmail.com>
2026-06-09 12:38:21 +0530

    Create components/Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+  return (
+    <aside className="w-64 h-screen border-r p-4">
+      <ul className="space-y-4">
+        <li>Dashboard</li>
+        <li>AI Chat</li>
+        <li>Documents</li>
+        <li>History</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
